7c7ea578d5f3ed1ea2c3649958d6f4d16de7c437
Mayra Zermeno <mayfassmich@gmail.com>
2022-11-24 09:45:50 -0600

    Js Proyect

---
+++ main.js
@@ -0,0 +1,15 @@
+//Entregable 1 Calculadora de Calorías
+
+let nombre = prompt('¡Hola! Ingresa tú nombre popr favor')
+alert = ('Bienvenido' + nombre)
+
+let edad = parseIntprompt('Ahora por favor ingresa tu edad')
+
+let actividadFisica = prompt('Cuantas horas haces de Actividad Física ')
+    //if 10min
+    //if 15min
+    //if 30min
+    //if 1-2 hrs
+    //if +de 3 horas 
+    //diario
+    //de vez en cuando
